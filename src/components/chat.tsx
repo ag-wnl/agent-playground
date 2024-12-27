132aaa6f24be50e253e34a55cd7b4e198d6f27e1
Agnideep Ghosh <agwnl@Agnideeps-MacBook-Air.local>
2024-12-28 00:41:47 +0530

    first commit

---
+++ src/components/chat.tsx
@@ -0,0 +1,9 @@
+export default function Chat() {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <h2 className="text-2xl font-bold">
+        This is a simple component on the right side
+      </h2>
+    </div>
+  );
+}
