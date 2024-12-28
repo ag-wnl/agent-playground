caf423d60c7a75d15b390f2a2d0bfc2ccc9eea16
ag-wnl <agnideep22@gmail.com>
2024-12-28 23:21:18 +0530

    mmove to monaco editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -3,30 +3,46 @@
 import { useCharacterValue } from "@/contexts/character";
 import { Character } from "@/contexts/type";
 import dynamic from "next/dynamic";
+import { useState } from "react";
 
-const JSONEditor = dynamic(
-  () => import("react-json-editor-ajrm").then((mod) => mod.default),
-  {
-    ssr: false,
-    loading: () => <p>Loading editor...</p>,
-  }
-);
+const MonacoEditor = dynamic(() => import("@monaco-editor/react"), {
+  ssr: false,
+  loading: () => <p>Loading editor...</p>,
+});
 
 export default function JSONEditorComponent() {
   const { character, setCharacter } = useCharacterValue();
 
-  const handleJsonChange = ({ jsObject }: { jsObject: Character }) => {
-    setCharacter(jsObject);
+  const [json, setJson] = useState(JSON.stringify(character, null, 2));
+
+  const handleEditorChange = (value: string | undefined) => {
+    if (value) {
+      setJson(value);
+
+      try {
+        const parsedJson: Character = JSON.parse(value);
+        setCharacter(parsedJson);
+      } catch (error) {
+        console.error("Invalid JSON:", error);
+      }
+    }
   };
 
   return (
     <div className="w-full h-full">
-      <JSONEditor
-        placeholder={character}
-        onChange={handleJsonChange}
-        locale="en"
+      <MonacoEditor
         height="100%"
-        width="100%"
+        language="json"
+        theme={"light"}
+        value={json}
+        onChange={handleEditorChange}
+        options={{
+          minimap: { enabled: false },
+          fontSize: 14,
+          wordWrap: "on",
+          wrappingIndent: "indent",
+          automaticLayout: true,
+        }}
       />
     </div>
   );
